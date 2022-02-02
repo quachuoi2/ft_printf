7a2d3480c55441925af1e8765c8fdad8c445661c
qnguyen <qnguyen@student.42.fr>
2022-02-02 15:46:06 +0200

    old check_prefix

--- prefix.c
+++ prefix.c
@@ -6,7 +6,7 @@
 /*   By: qnguyen <qnguyen@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/30 11:17:10 by qnguyen           #+#    #+#             */
-/*   Updated: 2022/02/02 15:33:36 by qnguyen          ###   ########.fr       */
+/*   Updated: 2022/02/02 15:45:51 by qnguyen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,21 +35,25 @@ void	check_prefix(char *fmt, t_order *order)
 {
 	while (*fmt != (*order).conv)
 	{
-		if ((*order).zero == 0 && (*order).mfw == 0)
+		if ((*order).zero == 0 && (*order).mfw == 0 && (*order).prec == 0)
 		{
 			if ((*order).conv == 'd' || (*order).conv == 'i' || (*order).conv == 'f')
 			{
-				(*order).pos = (*fmt == '+');
-				(*order).space = (*fmt == ' ');
+				if (*fmt == '+')
+					(*order).pos = 1;
+				else if (*fmt == ' ')
+					(*order).space = 1;
 			}
 			if (*fmt == '#' && ((*order).conv == 'x' || (*order).conv == 'X' || (*order).conv == 'o'))
-				(*order).hash = 2 - ((*order).conv == 'o');
-			(*order).neg = (*fmt == '-');
+				(*order).hash = 1;
+			else if (*fmt == '-')
+				(*order).neg = 1;
+			else if (*fmt == '0')
+				(*order).zero = 1;
 		}
-		(*order).zero = (*fmt == '0' && (*order).prec == 0);
-		if (*fmt >= '1' && *fmt <= '9' && (*order).prec == 0)
+		else if (*fmt >= '1' && *fmt <= '9' && (*order).prec == 0)
 			(*order).mfw = (*fmt) - '0';
-		if (*fmt == '.')
+		else if (*fmt == '.')
 			(*order).prec = 999;
 		fmt++;
 	}
